57db7e4be015632800e525324d450b89fe1b8928
koog7 <k.ognev709@mail.com>
2024-06-01 13:24:52 +0600

    Добавлена функция отобраÐжиение заказÐов и так Ðже добавлена глушилка

--- src/Components/OrderDetails.tsx
+++ src/Components/OrderDetails.tsx
@@ -1,7 +1,20 @@
-const OrderDetails = () => {
+const OrderDetails = ({ order }: { order: { name: string; count: number }[] }) => {
     return (
         <div>
-
+            <fieldset className={'OrderField'}>
+                <legend>Order Details: </legend>
+                {order.length === 0 ? (
+                    <p>Order empty, add something</p>
+                ) : (
+                    <ul>
+                        {order.map((item) => (
+                            <li key={item.name}>
+                                {item.name}: {item.count}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </fieldset>
         </div>
     );
 };
